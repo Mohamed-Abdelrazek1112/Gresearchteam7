ca410f8b1e70c7597a44c83aac0029c8e62787a3
Abhijay Sahay <abhijaygame@gmail.com>
2021-11-08 19:08:55 +0000

    1 2 13

--- python/rpnevaluator.py
+++ python/rpnevaluator.py
@@ -1,4 +1,5 @@
 import webhandler
+import re
 
 class RpnEvaluator(object):
     def __init__(self):
@@ -6,5 +7,28 @@ class RpnEvaluator(object):
 
     def evaluate_rpn(self, rpn):
         """Evaluate RPN"""
-        # TODO: add your code here
-        return 85
+
+        tokens = rpn.split()
+        stack = []
+        for token in tokens:
+            if token.isdecimal():
+                stack.append(int(token))
+            elif token == '+':
+                n2 = stack.pop()
+                n1 = stack.pop()
+                stack.append(n1 + n2)
+            elif token == '*':
+                n2 = stack.pop()
+                n1 = stack.pop()
+                stack.append(n1 * n2)
+            elif token == '-':
+                n2 = stack.pop()
+                n1 = stack.pop()
+                stack.append(n1 - n2)
+            elif token == '/':
+                n2 = stack.pop()
+                n1 = stack.pop()
+                stack.append(n1 // n2)
+
+        return stack.pop()
+        
