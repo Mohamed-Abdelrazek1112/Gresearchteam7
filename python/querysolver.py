2335a39e4c363a975891f040a430ecdf7316ed88
Abhijay Sahay <abhijaygame@gmail.com>
2021-11-08 18:44:09 +0000

    challenge 1

--- python/querysolver.py
+++ python/querysolver.py
@@ -1,4 +1,8 @@
 import webhandler
+import re
+
+def match_query(regex, query):
+    return len(re.findall(regex, query)) > 0
 
 class QuerySolver(object):
     def __init__(self):
@@ -6,5 +10,19 @@ class QuerySolver(object):
 
     def answer_query(self, query):
         """Answer a query"""
-        # TODO: add your code here
+        if match_query(r'^\d+ \d+ [\*\+]$', query):
+            return self._rpn1(query)
+
         return 85
+
+    def _rpn1(self, query):
+        m = re.match(r'^(\d+) (\d+) ([\*\+])$', query)
+        groups = m.groups()
+        n1 = int(groups[0])
+        n2 = int(groups[1])
+        op = groups[2]
+
+        if op == '+':
+            return n1 + n2
+        else:
+            return n1 * n2
